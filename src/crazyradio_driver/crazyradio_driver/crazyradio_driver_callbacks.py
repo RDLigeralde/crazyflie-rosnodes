6edac833279b65d2123e239e8de3562da16ca8bd
Lorenzo Bianchi <lnz.bnc@gmail.com>
2025-06-09 22:33:24 -0400

    fix: crazyradio_driver

--- src/crazyradio_driver/crazyradio_driver/crazyradio_driver_callbacks.py
+++ src/crazyradio_driver/crazyradio_driver/crazyradio_driver_callbacks.py
@@ -4,8 +4,9 @@ from cflib.crazyflie import Crazyflie
 from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
 
 def cmd_clbk(self, msg: CommandCTBR):
-    scf = self.scf_dict[msg.crazyflie_name]
-    scf.cf.commander.send_setpoint(msg.roll_rate, msg.pitch_rate, msg.yaw_rate, msg.thrust_pwm)  # FIXME: minus sign on yaw
+    if msg.crazyflie_name in self.scf_dict:
+        scf = self.scf_dict[msg.crazyflie_name]
+        scf.cf.commander.send_setpoint(msg.roll_rate, msg.pitch_rate, msg.yaw_rate, msg.thrust_pwm)  # FIXME: minus sign on yaw
 
 # def logger_clbk(self):
 #     for log_entry in self.sync_logger.next():
@@ -33,6 +34,9 @@ def reconnect_clbk(self):
 
                 self.scf_dict[crazyflie_name] = SyncCrazyflie(URI, cf=Crazyflie(rw_cache='./cache'))
                 self.scf_dict[crazyflie_name].open_link()
+
+                self.get_logger().warn('Sending zero command to %s' % crazyradio_uri)
+                self.scf_dict[crazyflie_name].cf.commander.send_setpoint(0.0, 0.0, 0.0, 0)
                 self.get_logger().warn('Connected to %s' % crazyradio_uri)
             except Exception as e:
                 continue
